98959565fd68f636e4a8d6df76aba152f0e8a826
sammy64201 <157268945+sammy64201@users.noreply.github.com>
2025-02-17 22:55:35 -0600

    Add files via upload

--- main.c
+++ main.c
@@ -1,54 +1,134 @@
 /*
- * Lab3 Digital2.c
+ * D2-Lab-03.c
  *
- * Created: 11/02/2025 13:54:28
- * Author : pablo
+ * Created: 2/11/2025 10:09:14 AM
+ * Author : samue
  */ 
-#define F_CPU 16000000UL
-#include <avr/io.h>
-#include <util/delay.h>
-#include <avr/interrupt.h>
-#include <stdio.h>
-#include <stdlib.h>
 
-#include "ADC/ADC.h"
+#define F_CPU 16000000UL
+#include <util/delay.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdint.h>
 #include "SPI/SPI.h"
 #include "UART/UART.h"
+#define BUFFER_SIZE 32
 
-char buffer[10];
+volatile char uart_buffer[BUFFER_SIZE];
+volatile uint8_t uart_head = 0;
+volatile uint8_t uart_tail = 0;
 
-void mostrarEnLEDS(uint8_t valor);
+volatile uint8_t p1 = 0;
+uint8_t p2 = 0;
+char buffer[30];
+float volt1;
+float volt2;
 
-int main(void)
-{
-    uint8_t comando;
-    spiInit(SPI_SLAVE);
-    initADC();
-    iniciarADC(0);
-
-    // Habilitar Puerto D como salida
-    DDRD = 0xFF;  // Configurar todos los pines del Puerto D como salida
-    PORTD = 0x00; // Inicializar LEDs apagados
-
-    sei(); // Habilitar interrupciones globales
-
-    while (1) 
-    {
-        comando = spiRead();  // Leer comando del maestro
-
-        if (comando == 1) {
-            spiWrite(pot1);
-        } 
-        else if (comando == 2) {
-            spiWrite(pot2);
-        } 
-        else if (comando == 3) {
-            uint8_t mensajeSPI = spiRead(); // Recibir un solo byte por SPI
-            mostrarEnLEDS(mensajeSPI);  // Mostrar el mensaje en los LEDs del Puerto D
-        }
-    }
+void Puertos(uint8_t ports) {
+	//PORTD = (PORTD & 0b00000011) | ((ports & 0b00001100) << 2);
+	//PORTB = (PORTB & 0b11111100) | ((ports & 0b11110000) >> 4) | (ports & 0b00000011);
+	PORTD = (PORTD & 0x03) | ((ports & 0x3F) << 2);
+	// D6-D7
+	PORTB = (PORTB & 0xFC) | ((ports >> 6) & 0x03);
 }
 
-void mostrarEnLEDS(uint8_t valor) {
-    PORTD = valor;  // Enviar el valor al puerto D
+void setup() {
+	cli();
+	PORTB |= (1 << PORTB2);
+	spiInit(SPI_MASTER_OSC_DIV16, SPI_DATA_ORDER_MSB, SPI_CLOCK_IDLE_LOW, SPI_CLOCK_FIRST_EDGE);
+	initUART9600();
+	DDRD |= 0b11111100; // PD2 - PD7 como salida
+	DDRB |= 0b00000011; // PB0 - PB1 como salida
+	UCSR0B |= (1 << RXCIE0); // Habilitar interrupción UART RX
+	sei();
+}
+
+ISR(USART_RX_vect) {
+	char c = UDR0;
+	uint8_t next_head = (uart_head + 1) % BUFFER_SIZE;
+	if (next_head != uart_tail) {
+		uart_buffer[uart_head] = c;
+		uart_head = next_head;
+	}
+}
+
+char recieveUART_Buffered() {
+	while (uart_head == uart_tail);
+	char c = uart_buffer[uart_tail];
+	uart_tail = (uart_tail + 1) % BUFFER_SIZE;
+	return c;
+}
+
+void Menu_PostLab() {
+	WriteTextUART("1. Leer el potenciometro\r\n");
+	WriteTextUART("2. Enviar valor entre 0 - 256\r\n");
+}
+
+
+
+void Leer_Potenciometros() {
+	PORTB &= ~(1 << PORTB2); // Activar esclavo SPI
+
+	// POTENCIOMETRO 1
+	spiWrite(1);
+	p1 = spiRead();
+	
+	// POTENCIOMETRO 2
+	spiWrite(2);
+	p2 = spiRead();
+
+	PORTB |= (1 << PORTB2); // Desactivar esclavo SPI
+
+	// CONVERSION PARA VOLTAJE 
+	volt1 = (p1 / 255.0) * 5.0;
+	volt2 = (p2 / 255.0) * 5.0;
+
+	// CONVERTIR PARA MANDAR EN UART
+	char buffer1[30], buffer2[30];
+	dtostrf(volt1, 5, 2, buffer1);
+	dtostrf(volt2, 5, 2, buffer2);
+
+	// MANDO POR UART 
+	sprintf(buffer, "Potenciometro 1: %sV\r\nPotenciometro 2: %sV\r\n", buffer1, buffer2);
+	WriteTextUART(buffer);
+}
+
+void Enviar_Valor_UART_SPI() {
+	WriteTextUART("Ingrese un numero (0-256): ");
+	char num_str[4];
+	uint8_t i = 0;
+	char c;
+	do {
+		c = recieveUART_Buffered();
+		if (c >= '0' && c <= '9' && i < 3) {
+			num_str[i++] = c;
+			writeUART(c);
+		}
+	} while (c != '\r');
+	num_str[i] = '\0';
+	uint8_t numero = atoi(num_str);
+	WriteTextUART("\r\n");
+
+	Puertos(numero);
+	WriteTextUART("Valor enviado al esclavo\r\n");
+	PORTB &= ~(1 << PORTB2);
+	spiWrite(3);
+	spiWrite(numero);
+	PORTB |= (1 << PORTB2);
+}
+
+int main(void) {
+	setup();
+	while (1) {
+		Menu_PostLab();
+		char opcion = recieveUART_Buffered();
+		writeUART(opcion);
+		WriteTextUART("\r\n");
+
+		if (opcion == '1') {
+			Leer_Potenciometros();
+			} else if (opcion == '2') {
+			Enviar_Valor_UART_SPI();
+		}
+	}
 }
